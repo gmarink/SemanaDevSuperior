54b70fa01239a0d52f3679d38dbdaf9e688ded25
Geraldo Marink <gmarink@gmail.com>
2021-05-06 20:51:26 -0400

    Basic static components

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="footer mt-auto bg-light">
+        <footer className="footer mt-auto py-3 bg-light">
         <div className="container">
           <p className="text-muted">App desenvolvido por <a href="https://github.com/gmarink" target="_blank" rel="noreferrer" className="text-muted">GMarink</a></p>
           <p className="text-muted"><small><strong>Semana Spring React</strong><br/>
