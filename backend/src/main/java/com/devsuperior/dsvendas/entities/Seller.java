df9f87d2de1fa24bdd1ed2ceaf181f09af2e9339
Geraldo Marink <gmarink@gmail.com>
2021-05-08 19:24:04 -0400

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_sellers")
+@Data
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales;
+}
